4014a150b26805ea6582656c39068ae90c2b6577
krishna <krishnaprasannakumar21@gmail.com>
2023-04-04 18:16:08 +0100

    Fix Bug

--- backend/index.js
+++ backend/index.js
@@ -31,3 +31,12 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+const PORT = process.env.port || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));

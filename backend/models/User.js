357bc301a206427f7b198b9f95fcba333cbd23b9
krishna <krishnaprasannakumar21@gmail.com>
2023-04-06 12:41:30 +0100

    Update User Model Errors

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,26 +4,26 @@ const UserSchema = new mongoose.Schema(
     {
         firstName: {
             type: String,
-            required: true,
-            minlength: 2,
-            maxlength: 50,
+            required: [true, "Please enter a first name"],
+            minlength: [2, "First Name must be longer than 2 characters"],
+            maxlength: [50, "First Name must be shorter than 50 characters"],
         },
         lastName: {
             type: String,
-            required: true,
-            minlength: 2,
-            maxlength: 50,
+            required: [true, "Please enter a last name"],
+            minlength: [2, "Last Name must be longer than 2 characters"],
+            maxlength: [50, "Last Name must be shorter than 50 characters"],
         },
         email: {
             type: String,
-            required: true,
-            maxlength: 50,
+            required: [true, "Please enter an email"],
+            maxlength: [50, "Email must be shorter than 50 characters"],
             unique: true,
         },
         password: {
             type: String,
-            required: true,
-            minlength: 5
+            required: [true, "Please enter a password"],
+            minlength: [5, "Password must be longer than 5 characters"]
         }
     }, { timestamps: true }
 );

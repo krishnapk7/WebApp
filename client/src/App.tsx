88f1e53c4544b09900efe446bb4995aaf1de89b4
krishna <krishnaprasannakumar21@gmail.com>
2023-04-11 16:11:46 +0100

    Prevent Unauthorised Access

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import "./App.css";
+import { AuthProvider, RequireAuth } from "react-auth-kit";
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+        >
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<RegisterScreen />} />
           <Route path="login/" element={<LoginScreen />} />
-          <Route path="home/" element={<HomeScreen />} />
+          <Route path="home/" element={<RequireAuth loginPath='/login'><HomeScreen /></RequireAuth>} />
         </Routes>
       </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }

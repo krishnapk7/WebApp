253b59a5715d8786909c052a3bf7d4f3ff05e0c5
krishna <krishnaprasannakumar21@gmail.com>
2023-04-12 14:47:10 +0100

    Add Sign Up using Auth0

--- client/src/screens/StartScreen.tsx
+++ client/src/screens/StartScreen.tsx
@@ -4,10 +4,22 @@ export default function StartScreen() {
   const { loginWithRedirect } = useAuth0();
   const { logout } = useAuth0();
 
+  const handleSignUp = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: window.location.origin,
+      },
+      authorizationParams: {
+        screen_hint: "signup",
+      },
+    });
+  };
+
   return (
     <div>
       <button onClick={() => loginWithRedirect()}>TEST</button>
       <button onClick={() => logout()}>Log Out</button>
+      <button onClick={handleSignUp}>Sign Up</button>
     </div>
   );
 }

c5e892d15e2e8c6fbe808b16eaccd1419b150cde
krishna <krishnaprasannakumar21@gmail.com>
2023-04-15 10:45:18 +0100

    Add User Name and Profile Picture

--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const HomeScreen = () => {
-  const { logout } = useAuth0();
+  const { logout, user } = useAuth0();
 
   return (
     <div>
-      <h1>HOME PAGE</h1>
+      <h1>Welcome {user?.name}</h1>
+      <img src={user?.picture} />
       <button onClick={() => logout()}>Log Out</button>
     </div>
   );
--- client/src/screens/StartScreen.tsx
+++ client/src/screens/StartScreen.tsx
@@ -13,7 +13,6 @@ export default function StartScreen() {
       },
     });
   };
-
   return (
     <div>
       <button onClick={() => loginWithRedirect()}>Log In</button>

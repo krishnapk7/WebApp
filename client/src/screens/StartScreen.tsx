98b4bdd8906956ea7e11c4bf1eee229c1ab7f650
krishna <krishnaprasannakumar21@gmail.com>
2023-05-15 11:14:19 +0100

    Add Tailwind Buttons

--- client/src/screens/StartScreen.tsx
+++ client/src/screens/StartScreen.tsx
@@ -14,9 +14,19 @@ export default function StartScreen() {
     });
   };
   return (
-    <div>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
-      <button onClick={handleSignUp}>Sign Up</button>
+    <div className="h-screen flex items-center justify-center">
+      <button
+        onClick={() => loginWithRedirect()}
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Log In
+      </button>
+      <button
+        onClick={handleSignUp}
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Sign Up
+      </button>
     </div>
   );
 }

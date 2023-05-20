03b3f4ba97f85a1cf4c1c640c98225f53ea38af3
krishna <krishnaprasannakumar21@gmail.com>
2023-05-20 12:57:03 +0100

    feat: add note model

---
+++ server/models/Model.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface NoteInterface {
+  name: string;
+  text: string;
+}
+
+export interface NoteModel extends NoteInterface, Document {}
+
+const NoteSchema: Schema = new mongoose.Schema({
+  name: { type: String, required: true, unique: false },
+  text: { type: String, required: true, unique: false },
+});
+
+export default mongoose.model<NoteModel>("User", NoteSchema);

49721671b1348a36f5512ea894ce69adddea32b2
krishna <krishnaprasannakumar21@gmail.com>
2023-05-21 20:49:14 +0100

    feat: addNotes screen styling

--- client/src/screens/AddNoteScreen.tsx
+++ client/src/screens/AddNoteScreen.tsx
@@ -1,3 +1,31 @@
 export default function AddNoteScreen() {
-  return <h1>Add Note Screen</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            id="name"
+          ></input>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Text
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            id="text"
+          ></input>
+        </div>
+        <button className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -29,7 +29,12 @@ const HomeScreen = () => {
       <Navbar />
       <h1>Welcome {user?.name}</h1>
       <img src={user?.picture} />
-      <button onClick={() => navigate("/addNote")}>Add Note</button>
+      <button
+        onClick={() => navigate("/addNote")}
+        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+      >
+        Add Note
+      </button>
     </div>
   );
 };

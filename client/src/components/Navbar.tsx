eb5e994dc347200d15310be6abcef939a1bac54e
krishna <krishnaprasannakumar21@gmail.com>
2023-06-01 22:08:34 +0100

    feat: add note to navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -9,6 +9,9 @@ export default function () {
         <NavLink className="text-2xl font-bold text-gray-50" to={"/home"}>
           WebApp
         </NavLink>
+        <NavLink className=" text-gray-50" to={"/addNote"}>
+          Add Note
+        </NavLink>
         <button
           onClick={() => logout()}
           className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"

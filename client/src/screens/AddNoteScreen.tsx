6806a21b935e575a59f1f7b994ef9c49cbd48b4f
krishna <krishnaprasannakumar21@gmail.com>
2023-06-22 13:49:01 +0100

    feat: send image  to backend

--- client/src/screens/AddNoteScreen.tsx
+++ client/src/screens/AddNoteScreen.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export default function AddNoteScreen() {
   const { user } = useAuth0();
   const [showModal, setShowModal] = useState(false);
-  const [image, setImage] = useState<string | ArrayBuffer | null>(null);
+  const [image, setImage] = useState<File>();
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
     const nameInput = document.getElementById("name") as HTMLInputElement;
@@ -17,10 +17,11 @@ export default function AddNoteScreen() {
       name: nameInput.value,
       text: textInput.value,
       user: user?.email,
-      image: image,
+      image: URL.createObjectURL(image),
     };
     const res = await auth_post("/note/addNote", body);
     setShowModal(true);
+    console.log(image);
     console.log(res.body);
   };
 
@@ -67,13 +68,22 @@ export default function AddNoteScreen() {
                   event.target.files &&
                   event.target.files[0]
                 ) {
-                  reader.onloadend = () => {
-                    setImage(reader?.result);
-                  };
+                  // reader.onloadend = () => {
+                  //   setImage(reader?.result);
+                  // };
+                  setImage(event.target.files[0]);
+                  console.log(URL.createObjectURL(event.target.files[0]));
                   reader.readAsDataURL(event.target.files[0]);
                 }
               }}
             />
+            {image && (
+              <img
+                alt="not found"
+                width="250px"
+                src={URL.createObjectURL(image)}
+              />
+            )}
           </div>
           <button
             className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"

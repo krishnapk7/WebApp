ceac21b9212d8d1708870857ad2801ca390b3cb8
krishna <krishnaprasannakumar21@gmail.com>
2023-07-03 14:14:26 +0100

    feat: different modal text for when note creation fails

--- client/src/screens/AddNoteScreen.tsx
+++ client/src/screens/AddNoteScreen.tsx
@@ -31,8 +31,11 @@ export default function AddNoteScreen() {
     }
 
     const res = await auth_post("/note/addNote", body);
-    console.log(res.body);
-    setModalText("Test");
+    if (res.status == 200) {
+      setModalText("Note Successfully Added!");
+    } else {
+      setModalText("Please Fill in all the required parts and try again!");
+    }
     setShowModal(true);
     console.log(image);
     console.log(res.body);
@@ -48,7 +51,7 @@ export default function AddNoteScreen() {
         >
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Name
+              Name*
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -58,7 +61,7 @@ export default function AddNoteScreen() {
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Text
+              Text*
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -97,6 +100,11 @@ export default function AddNoteScreen() {
                 src={URL.createObjectURL(image)}
               />
             )}
+          </div>
+          <div>
+            <p className="block text-gray-700 text-sm font-bold mb-2">
+              * required
+            </p>
           </div>
           <button
             className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"

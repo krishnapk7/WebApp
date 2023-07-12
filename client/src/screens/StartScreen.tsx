2f86a3787269e517dab9ed3a46f4b70ec9040251
krishna <krishnaprasannakumar21@gmail.com>
2023-07-12 21:47:56 +0100

    feat: improve landing page styling

--- client/src/screens/StartScreen.tsx
+++ client/src/screens/StartScreen.tsx
@@ -14,19 +14,27 @@ export default function StartScreen() {
     });
   };
   return (
-    <div className="h-screen flex items-center justify-center">
-      <button
-        onClick={() => loginWithRedirect()}
-        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      >
-        Log In
-      </button>
-      <button
-        onClick={handleSignUp}
-        className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-      >
-        Sign Up
-      </button>
+    <div className="container my-24 mx-auto md:px-6">
+      <p className="mb-12 text-5xl font-bold tracking-tight md:text-6xl xl:text-7xl text-center">
+        Welcome to WebApp!
+      </p>
+      <p className="mb-12 text-2xl font-bold tracking-tight md:text-xl xl:text-xl text-center">
+        Sign Up Now to start adding your own notes.
+      </p>
+      <div className="h-screen flex items-center justify-center">
+        <button
+          onClick={() => loginWithRedirect()}
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Log In
+        </button>
+        <button
+          onClick={handleSignUp}
+          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 }
